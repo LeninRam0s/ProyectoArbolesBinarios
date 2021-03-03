1846c76efc764f4e1f230b42e587a815bf3a4d15
LeninRam0s <djalexander.rmix@gmail.com>
2021-03-02 22:27:22 -0600

    Creacion de Clases, Arbol, Nodo, Adivina y MainAgregar archivos de proyecto.

---
+++ Clases/ArbolBinario/Arbol.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoArbolesBinarios.Clases.ArbolBinario
+{
+    class Arbol
+    {
+        protected Nodo raiz;//PROPIEDAD
+
+        public Arbol()
+        {
+            raiz = null;
+        }
+
+        public Arbol(Nodo valorRaiz)
+        {
+            this.raiz = valorRaiz;
+        }
+
+        public Nodo rizArbol()
+        {
+            return raiz;
+        }
+
+        bool esVacio()
+        {
+            return raiz == null;
+        }
+
+        public static Nodo nuevoArbol(Nodo ramaIzqda, object dato, Nodo ramaDrercha)
+        {
+            return new Nodo(ramaIzqda, dato, ramaDrercha);//DEVUELVE EL OBJETO NODO
+        }
+
+    }
+}
---
+++ Clases/ArbolBinario/Nodo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoArbolesBinarios.Clases.ArbolBinario
+{
+    class Nodo
+    {
+        public object dato;
+        public Nodo izquierda;
+        public Nodo derecha;
+
+        //CONSTRUCTOR
+        public Nodo(object valor)
+        {
+            dato = valor;
+            izquierda = null;
+            derecha = null;
+        }
+
+        //CONSTRUCTOR
+        public Nodo(Nodo ramaIzquierda, object valor, Nodo ramaDerecha)
+        {
+            dato = valor;
+            izquierda = ramaIzquierda;
+            derecha = ramaDerecha;
+        }
+
+        public void visitar()
+        {
+            Console.WriteLine(dato + "<->");
+        }
+
+        public object valorNodo()
+        {
+            return dato;
+        }
+
+        public Nodo subarbolDerecho()
+        {
+            return derecha;
+        }
+
+        public Nodo subarbolIzquierda()
+        {
+            return izquierda;
+        }
+
+        public void nuevoValor(object nw)
+        {
+            dato = nw;
+        }
+
+        public void ramaIZQ(Nodo n)
+        {
+            izquierda = n;
+        }
+
+        public void ramaDER(Nodo n)
+        {
+            derecha = n;
+        }
+
+    }
+}
